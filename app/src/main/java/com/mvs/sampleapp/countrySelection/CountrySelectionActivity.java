ee43040b7f3fe0e9b50e5f12c0cace66dfdfebb3
Snake <Maisam.vahidsafa@live.com>
2018-04-24 07:00:30 +0430

    Renamed: main activity and it's layout

--- app/src/main/java/com/mvs/sampleapp/countrySelection/CountrySelectionActivity.java
+++ app/src/main/java/com/mvs/sampleapp/countrySelection/CountrySelectionActivity.java
@@ -1,13 +1,15 @@
-package com.mvs.sampleapp;
+package com.mvs.sampleapp.countrySelection;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.mvs.sampleapp.R;
+
+public class CountrySelectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_country_selection);
     }
 }
