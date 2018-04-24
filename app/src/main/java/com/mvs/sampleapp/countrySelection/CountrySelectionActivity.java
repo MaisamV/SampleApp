23a573c8adc10931fc10e5f3ae9b90c8987a4343
Snake <Maisam.vahidsafa@live.com>
2018-04-24 07:39:42 +0430

    Injected views using ButterKnife

--- app/src/main/java/com/mvs/sampleapp/countrySelection/CountrySelectionActivity.java
+++ app/src/main/java/com/mvs/sampleapp/countrySelection/CountrySelectionActivity.java
@@ -2,19 +2,27 @@ package com.mvs.sampleapp.countrySelection;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.mvs.sampleapp.R;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CountrySelectionActivity extends AppCompatActivity implements ICountrySelectionMvpView {
 
     private ICountrySelectionPresenter presenter;
+
+    @BindView(R.id.countrySelection_recycler)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_selection);
+        ButterKnife.bind(this);
 
         presenter = new CountrySelectionPresenter(this);
     }
